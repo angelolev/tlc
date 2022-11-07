2a17a85d3d24526c65c82a6adaf3488138a264a1
Angelo Leva <angelokta7@gmail.com>
2022-11-07 14:19:39 -0500

    fixed issues

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,4 @@
 $(document).ready(function () {
-    // Add smooth scrolling to all links
-    $(".scrollable").on("click", function (e) {
-      e.preventDefault();
-        var target = this.hash,
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 900, 'swing', function () {
-            window.location.hash = target;
-        });
-    });
-
     $(".scrollable").click(function() {
               
       var listItems = $(".scrollable");
@@ -22,7 +10,14 @@ $(document).ready(function () {
       this.classList.add("active");
     });
   });
-  
+
+  const scrollToElement = (element) => {
+    const destiny = document.querySelector(element)
+    destiny.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
+
   document.addEventListener("DOMContentLoaded", () => {
     function counter(id, start, end, duration) {
      let obj = document.getElementById(id),
